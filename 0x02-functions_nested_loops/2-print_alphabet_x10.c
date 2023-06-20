c5100c2e12a03d174be2f5cd59c896464359a2c5
Kofoworola98 <agbedekofo@gmail.com>
2023-06-20 13:39:00 -0700

    Print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
 void print_alphabet_x10(void)
 {
 	char num, n;
-
-        for (num = 0; num <= 9; num++)
-        {
+	
+	for (num = 0; num <= 9; num++)
+	{
 		for (n = 'a'; n <= 'z'; n++)
-                {
+		{
 			_putchar(n);
-                }
-                _putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
