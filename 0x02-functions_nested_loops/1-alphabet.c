9fce717dea175169982420b76c9c381983797bd3
Kofoworola98 <agbedekofo@gmail.com>
2023-06-20 13:05:20 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,18 @@
 
 /**
  * main - This function prints the alphabet in lower case.
- * Return:0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char c = 'a';
-	
+       
 	while (c <= 'z');
 	{
 		_putchar(c);
 		c++;
 	}
 	_putchar('\n');
+	return (0);
 }
