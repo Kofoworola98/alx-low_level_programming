2101e5579fc460afe2a71378a5b5b6af7cf8fb9f
Kofoworola98 <agbedekofo@gmail.com>
2023-06-20 18:12:44 -0700

    Fibonacci 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Check descripton
- * Description: This computes and prints the sum of all th multiples of 
+ * Description: This computes and prints the sum of all the multiples of
  * 3 or 5 below 1024, followed by a new line.
  * Return: 0
  */
 int main(void)
 {
 	int n = 0;
-	unsigned long int x = 0, y = 1, next =0;
+	unsigned long int x = 0, y = 1, next = 0;
 
 	while (n < 98)
 	{
