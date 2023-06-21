65db4f843d07b685e6b6fb5f324e5eb9e3e174da
Kofoworola98 <agbedekofo@gmail.com>
2023-06-20 18:36:57 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+	
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -16,7 +17,7 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				}else if (k < 10 && j != 0)
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
