443f6e7ce5aa1633525312f3f335f1a36f0da7f9
Kofoworola98 <agbedekofo@gmail.com>
2023-06-20 17:34:02 -0700

    Fiboacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	long int i, j, k, next;
-	
+
 	j = 1;
-	
+
 	k = 2;
-	
+
 	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
@@ -27,6 +27,6 @@ int main(void)
 		j = k;
 		k = next;
 	}
-	
+
 	return (0);
 }
