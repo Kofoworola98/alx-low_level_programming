41428b17c3645012f9e69cdb3b08c658134e4d7b
Kofoworola98 <agbedekofo@gmail.com>
2023-07-13 07:36:01 -0700

    Mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,7 +5,7 @@
 #define ERR_MSG "Error"
 
 /**
- * is_digit - This chec ks if a string contains a non digit char.
+ * is_digit - This checks if a string contains a non digit char.
  * @s: string to be evaluated
  *
  * Return: 0 if a non digit is found, 1 otherwie
