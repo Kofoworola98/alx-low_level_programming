801645351a544fe2f9220a37e9848c1d096d0213
Kofoworola98 <agbedekofo@gmail.com>
2023-07-13 07:45:05 -0700

    Mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -26,7 +26,7 @@ int is_digit(char *s)
 
 /**
  * _strlen - This function returns the length of a string.
- * @s: string to be evaluated
+ * @s: string to evaluate.
  *
  * Return: the length of the string
  */
