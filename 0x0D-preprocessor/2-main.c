dead533c5ac7796391d71cd06c35a90ad5e02d78
Kofoworola98 <agbedekofo@gmail.com>
2023-07-17 06:56:25 -0700

    2 Main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main: This function prints the name of the file it was compiled from.
+ * main - This function prints the name of the file it was compiled from.
  *
  * Return: always 0 (success)
  */
