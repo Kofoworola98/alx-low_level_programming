96d7802970389525cea43294878e59a19443da7d
Kofoworola98 <agbedekofo@gmail.com>
2023-07-17 08:05:55 -0700

    DOG

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,5 +24,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
-int _strlen(char*s);
+int _strlen(char *s);
 #endif
